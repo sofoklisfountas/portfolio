175381c435cfd869f59e76c7b5ceb9190721100d
Sofoklis <sofoklisfountas@gmail.com>
2019-10-30 20:59:09 +0100

    Minor changes to CSS top & bottom paddings in sections

--- contact-form.php
+++ contact-form.php
@@ -1,36 +1,38 @@
 <?php
 
 
-if(filter_has_var(INPUT_POST, 'submit')){
-    // get form data
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-
-    //Check Required Fields
-    if(!empty($email) && !empty($name) && !empty($message)){
-        //Passed
-        echo 'PASSED';
-    }else{
-        //Failed
-        $errorMsg = 'Please fill in all the fields';
-        $errorMsgClass = 'Error';
-
-    }
-}
-
-// if (isset($_POST['submit'])){
+// if(filter_has_var(INPUT_POST, 'submit')){
+//     // get form data
 //     $name = $_POST['name'];
-//     $mailFrom = $_POST['email'];
+//     $email = $_POST['email'];
 //     $message = $_POST['message'];
 
+//     //Check Required Fields
+//     if(!empty($email) && !empty($name) && !empty($message)){
+//         //Passed
+//         echo 'PASSED';
+//     }else{
+//         //Failed
+//         $errorMsg = 'Please fill in all the fields';
+//         $errorMsgClass = 'Error';
 
-//     $mailTo = "v3nomlink@gmail.com";
-//     $headers = "From: ".$mailFrom;
-//     $txt = "You have received an email from ".$name/.".\n\n".$message;
-
-//     mail($mailTo, $txt, $headers);
-//     header("Location: index.html?mailsend");
+//     }
 // }
+
+if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $message = $_POST['message'];
+
+
+    $mailTo = "v3nomlink@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an email from ".$name.".\n\n".$message;
+
+    if (mail($mailTo, $txt, $headers)){
+        $sent = true;
+    }
+    header("Location: index.html");
+}
 
 ?>
